2540cb4594b7e4bdf6dfe3cddf71cfd0631f5cf2
Cassio Kenji Oshiro Batista <cbatista.ext@vr.com.br>
2018-01-31 23:58:49 -0200

    refactor register_driver block

--- app.rb
+++ app.rb
@@ -2,14 +2,8 @@ require 'capybara'
 require 'selenium-webdriver'
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app,
-  :browser => :chrome,
-  :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
-        'chromeOptions' => {
-          'args' => [ "headless"]
-        }
-      )
-  )
+  options = Selenium::WebDriver::Chrome::Options.new(args: ['--headless'])
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, options: options)
 end
 
 Capybara.app_host = 'http://www.google.com'
